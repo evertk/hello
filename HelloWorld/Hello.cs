09834ccdc59830d358fd7e85a85e861c9399b021
Evert Kramer <evertk@deltainstruments.com>
2016-09-22 15:30:20 +0200

    Add project files.

---
+++ HelloWorld/Hello.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Hello.cs" company="Delta Instruments">Copyright © Delta Instruments B.V.</copyright>
+
+namespace HelloWorld
+{
+	class Hello
+	{
+		public Hello()
+		{
+
+		}
+
+		public string Text()
+		{
+			return "Hello World! Evertk is tha Man!" + Dummy();
+		}
+		
+		public string FreerksText()
+		{
+			return "Who's the man?";
+		}
+
+		public string Dummy()
+		{
+			return "bladiebla!";
+		}
+		
+		public string Dunny()
+		{
+			return "Eat this GIT";
+		}
+	}
+}
